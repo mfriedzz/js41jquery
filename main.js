5e1b845403a22d0bc66e5db0da25531f784d580d
Michael F <mfriedmanzz01@gmail.com>
2015-01-20 17:46:26 -0700

    here

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).on('ready', function() {
+  
+  $(".here").click(function () {
+  		alert("Welcome");
+  		// $( ".inner" ).append( "<p>Test</p>" );
+  		// $( ".container" ).append(  );
+ 	});
+
+ 	 	$(".here2").click(function () {
+
+  			$("body").append("<h1>Hello here!</h1>");
+
+ 		});
+
+				$(".here3").click(function () {
+
+  					$("body").append("<ul><li>child1</li><li>child2</li><li>child3</li></ul>");
+
+ 				});
+
+
+});
+
+
